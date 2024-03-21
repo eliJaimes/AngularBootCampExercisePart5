1981abb644d353f647d2fb7c3f0702116aefa595
Eli Jaimes <eli.jaimes.ramos@gmail.com>
2024-03-21 13:21:57 -0600

    Do clean up

--- NOTES.md
+++ NOTES.md
@@ -7,3 +7,9 @@ ng new ExercisePart5 --skip-tests --dry-run
 > Which stylesheet format would you like to use? SCSS
 > Do you want to enable Server-Side Rendering (SSR) and Static Site Generation (SSG/Prerendering)? N
 ```
+
+## Do clean up
+
+- Sort imports
+- Clean app.component.\* files
+- Remove unnecessary editor files
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
+/* ••[1]••••••••••••••••••••••••• app.component.ts •••••••••••••••••••••••••••••• */
+
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
+  imports: [],
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  styleUrl: './app.component.scss',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'ExercisePart5';
-}
+export class AppComponent {}
