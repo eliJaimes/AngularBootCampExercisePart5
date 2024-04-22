74093b8579809d6f9ce0b9134998003d327bbcb0
Eli Jaimes <eli.jaimes.ramos@gmail.com>
2024-04-22 11:34:18 -0600

    Create a signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 /* ••[1]••••••••••••••••••••••••• app.component.ts •••••••••••••••••••••••••••••• */
 
-import { Component } from '@angular/core';
+import { Component, WritableSignal, signal } from '@angular/core';
 
 @Component({
-  imports: [],
-  selector: 'app-root',
-  standalone: true,
-  styleUrl: './app.component.scss',
-  templateUrl: './app.component.html',
+	imports: [],
+	selector: 'app-root',
+	standalone: true,
+	styleUrl: './app.component.scss',
+	templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+	// This is how you define a signal
+	public readonly numberSignal: WritableSignal<number> = signal(5);
+
+	public readonly stringArraySignal: WritableSignal<Array<string>> = signal([
+		'Hello',
+		'World',
+		'!',
+	]);
+}
